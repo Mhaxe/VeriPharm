700a1e12274a824338b2fe4b6fbbae57e31b12e0
Mhaxe <mxwllhayibor@gmail.com>
2025-06-08 20:51:18 +0000

    changes to manufacturer role

--- manufacturer/admin.py
+++ manufacturer/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Batch,BatchDistribution,Drug
+from .models import Batch,BatchDistribution,Drug
 
 # Register your models here.
 admin.site.register(Batch)
